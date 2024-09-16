e45d51212a42b711c0354fd92cba224f6a5e328e
harshd3004 <desaiharsh512@gmail.com>
2024-09-16 11:04:13 +0530

    implemented router

---
+++ server/auth-router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//     res.send('Hello from router!');
+// }); 
+// OR
+router.route('/').get((req, res) => {
+    res.send('Hello from router!');
+});  
+
+module.exports = router
