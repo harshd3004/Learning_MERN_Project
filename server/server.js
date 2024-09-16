b726c470f310341ec7d41ce95eca00c066bc0da2
harshd3004 <desaiharsh512@gmail.com>
2024-09-16 11:45:44 +0530

    added nodemon tool

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const router = require('./router/auth-router');
 
-app.use('api/auth',router);
+app.use('/api/auth',router);
 
-// app.get('/',(req,res)=>{
-//     res.status(200).send("Hello world")
-// })
+app.get('/',(req,res)=>{
+    res.status(200).send("Hello world")
+})
 
 const port = 5000;
 app.listen(port,()=>{
